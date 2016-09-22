474f16e20c3136fc17c3efd47707d82bf50c7a29
Yousef Rabie <yousef.rabie@cis.asu.edu.eg>
2016-09-22 09:46:50 +0200

    First Commit

---
+++ Youtube C Sharp Tutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_C_Sharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] input = Console.ReadLine().Split();
+
+
+            if (int.Parse(input[0]) > int.Parse(input[1]))
+                Console.WriteLine(">");
+            else if (int.Parse(input[0]) > int.Parse(input[1]))
+                Console.WriteLine("<");
+            else
+                Console.WriteLine("=");
+        }
+    }
+}
